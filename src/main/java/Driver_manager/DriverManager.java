47fb4468d3fe4657a04ea9ded987697556e1cfa5
JajnyaseniTOP <jajnyaseni.swain@theoutsourcepro.com.au>
2024-10-24 12:16:06 +0530

    modified driber manager

--- src/main/java/Driver_manager/DriverManager.java
+++ src/main/java/Driver_manager/DriverManager.java
@@ -5,27 +5,50 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
- 
+
+import java.io.File;
+
 public class DriverManager {
-	
-	public static WebDriver driver;
-	
-	public static void setDriver(String browser) {
-		if(browser == "Chrome") {
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--headless=new");
-			options.addArguments("--no-sandbox");
-			driver = new ChromeDriver(options);
-			
-		}else {
-			FirefoxOptions options = new FirefoxOptions();
-			options.addArguments("--headless");
-			options.addArguments("--disable-gpu");
-			driver = new FirefoxDriver(options);
-		}
-	}
-	
-	public static WebDriver getDriver() {
-		return driver;
-	}
+
+    public static WebDriver driver;
+
+    public static void setDriver(String browser) {
+        // Get the Jenkins workspace directory
+        String downloadPath = System.getProperty("jenkins.home") + "/workspace/ATOEmail/downloads";
+
+        if (browser.equalsIgnoreCase("Chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+
+            // Set download path for Chrome
+            options.setExperimentalOption("prefs", new java.util.HashMap<String, Object>() {{
+                put("download.default_directory", downloadPath);
+                put("download.prompt_for_download", false);
+                put("download.directory_upgrade", true);
+                put("safebrowsing.enabled", true);
+            }});
+
+            driver = new ChromeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+
+            // Set download path for Firefox
+            options.addPreference("browser.download.dir", downloadPath);
+            options.addPreference("browser.download.folderList", 2); // 2 means use custom download directory
+            options.addPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf,application/octet-stream"); // Adjust MIME types as needed
+
+            driver = new FirefoxDriver(options);
+        }
+
+        // Ensure the download directory exists
+        new File(downloadPath).mkdirs();
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
 }
